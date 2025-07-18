820f8dbf0eb8bcff55900054f3fddf9ad34832f3
JayMatt21 <161000212+JayMatt21@users.noreply.github.com>
2025-07-18 23:51:17 +0800

    Remove venv folder from repo and add to .gitignore

--- app.py
+++ app.py
@@ -10,15 +10,34 @@ from numpy import dot
 from numpy.linalg import norm
 import pandas as pd
 from fpdf import FPDF
+from dotenv import load_dotenv
+load_dotenv()
+
+service_account_info = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
+
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'senfrostfr.firebasestorage.app'
+})
+
+import os
+print(os.getenv("FIREBASE_PROJECT_ID"))
+
 
 app = Flask(__name__)
 app.secret_key = 'nooneelse'
 
-# === Firebase ===
-cred = credentials.Certificate('serviceAccountKey.json')
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'senfrostfr.firebasestorage.app'
-})
 db = firestore.client()
 bucket = storage.bucket()
 
--- firebase_sync.py
+++ firebase_sync.py
@@ -1,18 +1,21 @@
-from firebase_admin import storage, credentials, initialize_app
 import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
 
-cred = credentials.Certificate("serviceAccountKey.json")
-initialize_app(cred, {
-    'storageBucket': 'senfrostfr.firebasestorage.app'
+load_dotenv()
+
+cred = credentials.Certificate({
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
 })
 
-bucket = storage.bucket()
-blobs = bucket.list_blobs(prefix="known_faces/")
-os.makedirs("known_faces", exist_ok=True)
-
-for blob in blobs:
-    if blob.name.endswith(".jpg"):
-        local_path = os.path.join("known_faces", os.path.basename(blob.name))
-        blob.download_to_filename(local_path)
-
-print("✅ Synced known_faces from Firebase.")
+firebase_admin.initialize_app(cred)
